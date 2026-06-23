097f4531d29813da375580cb789ac43002b73ba9
Saloni Tyagi <salonityagi2004@gmail.com>
2026-06-23 23:18:56 +0530

    Add complaint model

---
+++ models/Complaint.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const complaintSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: true
+    },
+
+    vehicleNumber: {
+        type: String
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Complaint", complaintSchema);
